e8e79b34e2eedebd57253ff56dc9fa44cc1f9157
Lillian Shieh <lillian.shieh95@gmail.com>
2018-04-05 21:59:37 -0400

    fixed log-in redirect

--- lilstagram/config/routes.rb
+++ lilstagram/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #get "/posts/:id", to: "posts#create_comment"
+  root to: "posts#index"
   resources :users, only: [:show]
   resources :posts do
   	resources :comments
